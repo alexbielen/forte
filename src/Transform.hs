131f4055438bc5ee69edb87921c7a7c6fcab3e9b
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2021-11-20 16:39:46 -0500

    Add a few series functions, namely xToDx and dxToX.

---
+++ src/Transform.hs
@@ -0,0 +1,112 @@
+module Transform
+  ( fit,
+    fitF,
+    xToDx,
+    dxToX,
+    uniformQuantize,
+    uniformQuantizeF,
+    FitMode (..),
+    UniformMode (..),
+  )
+where
+
+data FitMode
+  = Wrap
+  | Clamp
+  deriving (Show)
+
+-- |
+-- fit transforms a value to "fit" within a range according to a mode.
+--
+-- Wrap mode uses modular (aka "clock") arithmetic to "wrap" n into the range.
+-- This is expressed in the following equation:
+-- n' = n - floor((n - min) / (max - min )) *  (max - min)
+--
+-- Clamp mode takes any out-of-range values and replaces with the nearest bound.
+fit :: Integral a => FitMode -> a -> a -> a -> a
+fit mode lo hi n =
+  if inRange
+    then n
+    else case mode of
+      Wrap -> n - ((n - lo) `div` range) * range
+      Clamp -> nearBound
+  where
+    inRange = lo <= n && n <= hi
+
+    range = hi - lo
+
+    -- the bound that is closest to n
+    nearBound =
+      if n > hi
+        then hi
+        else lo
+
+-- |
+-- fitF maps the fit function over functors.
+fitF :: (Functor f, Integral a) => FitMode -> a -> a -> f a -> f a
+fitF mode min max = fmap (fit mode min max)
+
+-- |
+-- Converts a list of points into a list of intervals.
+--
+-- You might want to use this to calculate the intervals in a 12-tone row or to
+-- get the durations in a rhythmic series.
+--
+-- Example:
+--
+-- In: [5 1 4 2 3]
+-- Out: [-4 3 -2 1]
+xToDx :: (Num a) => [a] -> [a]
+xToDx [] = []
+xToDx [_] = []
+xToDx l@(_ : xs) = zipWith diff l xs
+  where
+    diff a b = b - a
+
+-- |
+-- Creates a list of points from a start value and a list of intervals.
+--
+-- You might want to use this to calculate a melody from a starting note and a list of intervals.
+dxToX :: (Num a) => a -> [a] -> [a]
+dxToX start = foldl (\acc x -> acc ++ [last acc + x]) [start]
+
+
+-- |
+-- QuantizeMode
+--
+-- MidTread
+-- MidRiser
+data UniformMode
+  = MidTread
+  | MidRiser
+  deriving (Show)
+
+-- |
+-- uniformQuantize transforms a Real number to the closest multiple of step. There are two modes:
+--
+-- MidTread uses the following algorithm:
+--     step * floor((n / step) + 0.5)
+--
+-- MidRiser uses the following algorithm:
+--     step * (floor(n / step)) + 0.5
+uniformQuantize :: (RealFrac a) => UniformMode -> a -> a -> a
+uniformQuantize mode step n = case mode of
+  MidTread -> step * midTreadClassificationStage
+  MidRiser -> step * (midRiserClassificationStage + 0.5)
+  where
+    midTreadClassificationStage = conv ((n / step) + 0.5)
+
+    midRiserClassificationStage = conv (n / step)
+
+    conv = fromIntegral . floor
+
+{-
+uniformQuantizeF maps the uniformQuantize function over functors.
+-}
+uniformQuantizeF :: (Functor f, RealFrac a) => UniformMode -> a -> f a -> f a
+uniformQuantizeF mode step = fmap (uniformQuantize mode step)
+
+-- |
+-- rotate a list by n.
+rotate :: Int -> [a] -> [a]
+rotate = drop <> take
