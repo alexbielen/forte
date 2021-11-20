484373d6398757b887027e60bbfaed55ca51219b
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2021-11-19 20:34:53 -0500

    Add default cabal project with a nix shell for development environment.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for forte
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ shell.nix
@@ -0,0 +1,28 @@
+let
+  myNixPkgs = import <nixpkgs> {
+    overlays = [myNixPkgsOverlay];
+  };
+
+  myNixPkgsOverlay = (nixSelf: nixSuper: {
+    myHaskellPackages = nixSelf.haskellPackages.override (oldHaskellPkgs: {
+      overrides = nixSelf.lib.composeExtensions (oldHaskellPkgs.overrides or (_: _: {}))  myHaskellPkgsOverlay;
+    });
+  });
+
+  myHaskellPkgsOverlay = (hSelf: hSuper: {
+    forte = hSelf.callCabal2nix "forte" ./. {};
+  });
+  
+  myDevTools = with myNixPkgs; [
+    cabal-install 
+    haskellPackages.ghcid
+  ];
+
+  myShellHook = ''
+    alias repl="cabal new-repl"
+  '';
+in
+myNixPkgs.myHaskellPackages.forte.env.overrideAttrs (oldEnv: {
+  nativeBuildInputs = oldEnv.nativeBuildInputs ++ myDevTools;
+  shellHook = myShellHook;
+})
